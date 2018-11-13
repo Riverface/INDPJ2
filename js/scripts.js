0efc037480fd431aabf32d04078e7ee04251e0d9
Akigawa <sonoskeakigawa@gmail.com>
2018-11-13 13:52:21 -0800

    asdf

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function()
 {
+  var AbomArray;
   var ans = new Array(0,0,0) ;
   ans[0] = 0;
   ans[1] = 0;
@@ -128,14 +129,36 @@ function abomination()
 {
 
 AbomArray = new Array(8);
-var AbomArray = ["images/Petz4thing.png", "images/petz4thing2.png", "images/petz4thing3.png", "images/petz4thing4.png", "images/petz4thing5.png", "images/petz4thing6.png", "images/Petz4thing7.png", "images/Petz4thing8.png"]
+AbomArray[0] = new Image();
+AbomArray[0].src = "images/Petz4thing.png";
+
+AbomArray[1] = new Image();
+AbomArray[1].src = "images/petz4thing2.png";
+
+AbomArray[2] = new Image();
+AbomArray[2].src = "images/petz4thing3.png";
+
+AbomArray[3] = new Image();
+AbomArray[3].src = "images/petz4thing4.png";
+
+AbomArray[4] = new Image();
+AbomArray[4].src = "images/petz4thing5.png";
+
+AbomArray[5] = new Image();
+AbomArray[5].src = "images/petz4thing6.png";
+
+AbomArray[6] = new Image();
+AbomArray[6].src = "images/Petz4thing7.png";
+AbomArray[6] = new Image();
+AbomArray[6].src = "images/Petz4thing8.png";
 
 var num = Math.floor(Math.random() * Math.floor(8));
 console.log(num);
-    console.log($('.abomination').attr("src"));
-    $('.abomination').src = "./images/Petz4thing.png";
+    console.log($('img#abomination').attr("src"));
+    $('#abomination').attr(AbomArray[num],);
     console.log(document.getElementById("abomination"));
-    console.log(AbomArray[num]);
-    console.log($(".abomination"));
- console.log($(".abomination").src);
+    console.log(AbomArray[num].src);
+    console.log($("#abomination"));
+
+ console.log($("#abomination").src);
 }
