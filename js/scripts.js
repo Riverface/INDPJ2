a36cd705131634d566da5b4ad25b25f7c6ab4f84
Akigawa <sonoskeakigawa@gmail.com>
2018-11-14 13:21:50 -0800

    zzzzzoop

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
+var ans = new Array(0,0,0) ;
 
 $(document).ready(function()
 {
   var AbomArray;
-  var ans = new Array(0,0,0) ;
   ans[0] = 0;
   ans[1] = 0;
   ans[2] = 0;
-$("#answercontainer").hide();
+  $("#answercontainer").hide();
 
   var qIterator = -1;
   var laugh = new Audio('audio/SkKid.wav');
-
+  $("#rresult").hide();
+  $("#wresult").hide();
   $("#result1").hide();
   $("#reject").click(function(){
     $("#intro").hide();
@@ -24,6 +25,37 @@ $("#answercontainer").hide();
   });
 
   $("#answr1").click(function(){
+    ans[0]++;
+    qIterator++;
+    switch (qIterator) {
+      case 0:
+      $("#qcontent").load("./questions.html .q2");
+      console.log(qIterator);
+      break;
+      case 1:
+      $("#qcontent").load("./questions.html .q3");
+      console.log(qIterator);
+      break;
+      case 2:
+      $("#qcontent").load("./questions.html .q4");
+      console.log(qIterator);
+      break;
+      case 3:
+      $("#qcontent").load("./questions.html .q5");
+      console.log("zoop");
+      break;
+      case 4:
+      $("#qcontent").load("./Results.html #result1");
+      console.log("zoop");
+      $("#result1").show();
+      $(".abomination").show();
+      $("#answercontainer").hide();
+
+      abomination();
+      break;
+    }
+  });
+  $("#answr2").click(function(){
     ans[1]++;
     qIterator++;
     switch (qIterator) {
@@ -47,40 +79,9 @@ $("#answercontainer").hide();
       $("#qcontent").load("./Results.html #result1");
       console.log("zoop");
       $("#result1").show();
-    $(".abomination").show();
+      $(".abomination").show();
       $("#answercontainer").hide();
-
-          abomination();
-      break;
-    }
-  });
-  $("#answr2").click(function(){
-    ans[2]++;
-    qIterator++;
-    switch (qIterator) {
-      case 0:
-      $("#qcontent").load("./questions.html .q2");
-      console.log(qIterator);
-      break;
-      case 1:
-      $("#qcontent").load("./questions.html .q3");
-      console.log(qIterator);
-      break;
-      case 2:
-      $("#qcontent").load("./questions.html .q4");
-      console.log(qIterator);
-      break;
-      case 3:
-      $("#qcontent").load("./questions.html .q5");
-      console.log("zoop");
-      break;
-      case 4:
-      $("#qcontent").load("./Results.html #result1");
-      console.log("zoop");
-      $("#result1").show();
-    $(".abomination").show();
-      $("#answercontainer").hide();
-    abomination();
+      abomination();
       break;
     }
 
@@ -107,8 +108,8 @@ $("#answercontainer").hide();
       break;
       case 4:
       $("#result1").show();
-    $(".abomination").show();
-    abomination();
+      $(".abomination").show();
+      abomination();
       console.log("zoop");
 
       $("#answercontainer").hide();
@@ -122,17 +123,21 @@ $("#answercontainer").hide();
 function wow()
 {
   $("#qcontent").show();
-    $("#result1").hide();
-    $("#qcontent").load("./Results.html #rresult");
+  $("#result1").hide();
+  $("#rresult").show();
+
+  $("#qcontent").load("./Results.html #rresult");
+  decide();
 
 
 }
 function thanksihateit()
 {
-    $("#qcontent").show();
-        $("#result1").hide();
-    $("#qcontent").load("./Results.html #wresult");
-
+  $("#qcontent").show();
+  $("#result1").hide();
+  $("#qcontent").load("./Results.html #wresult");
+  $("#wresult").show();
+  decide();
 }
 
 function abomination()
@@ -141,7 +146,8 @@ function abomination()
 
 
 
-$("#qcontent").hide();
+  $("#qcontent").hide();
+  {
     AbomArray = new Array(8);
     AbomArray[0] = new Image();
     AbomArray[0].src = "./images/Petz4thing.png";
@@ -165,13 +171,33 @@ $("#qcontent").hide();
     AbomArray[6].src = "./images/Petz4thing7.png";
     AbomArray[7] = new Image();
     AbomArray[7].src = "./images/Petz4thing8.png";
+  }
+  var num = Math.floor(Math.random() * Math.floor(8));
+  console.log(num);
 
-    var num = Math.floor(Math.random() * Math.floor(8));
-    console.log(num);
+  console.log($('Results.html .abomination').attr("src"));
+  var abomimg;
+  abomimg = AbomArray[num].src;
+  $('[name="canvas"]').attr("src", AbomArray[num].src);
 
-    console.log($('Results.html .abomination').attr("src"));
-    var abomimg;
-    abomimg = AbomArray[num].src;
-$('[name="canvas"]').attr("src", AbomArray[num].src);
+}
+
+function decide()
+{
+  if ((ans[2] <= ans[0]) && (ans[1] <= ans[0]) )
+  {
+        console.log(ans[1] + " " + ans[2] + " " + ans[0]);
+    $(".result3").load("Results.html .l1");
+  }
+  else if ((ans[2] <= ans[1]) && (ans[0] <= ans[1]) )
+  {
+        console.log(ans[1] + " " + ans[2] + " " + ans[0]);
+    $(".result3").load("Results.html .l2");
+  }
+  else if ((ans[1] <= ans[2]) && (ans[0] <= ans[2]) )
+  {
+    console.log(ans[1] + " " + ans[2] + " " + ans[0]);
+    $(".result3").load("Results.html .l3");
+  }
 
 }
